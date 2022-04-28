81af6a33c5119d02575d3fc5706f492b3c7712a4
临来笑笑生 <luck@elapse.date>
2022-04-28 11:29:20 +0800

    Squashed commit of the following:
    commit 9d50029ed41a8dddbb3488d54dcad7b05cb60ace
    Author: 临来笑笑生 <luck@elapse.date>
    Date:   Thu Apr 28 11:23:37 2022 +0800
    
        添加日志记录
    
    commit 31f2cda94a482ef301ee9c5f38725f5112be8c63
    Merge: 52d0b04 7ace94a
    Author: 临来笑笑生 <luck@elapse.date>
    Date:   Wed Apr 27 14:59:32 2022 +0800
    
        Merge branch 'main' into luck
    
    commit 52d0b04fe6499ec79680ef9fa10baa86b6f8158b
    Author: 临来笑笑生 <luck@elapse.date>
    Date:   Wed Apr 27 14:49:28 2022 +0800
    
        合并分支说明
    
    commit 9bd1897b4ac944a62bfb5283324b917a583ebf25
    Author: 临来笑笑生 <luck@elapse.date>
    Date:   Wed Apr 27 14:45:16 2022 +0800
    
        开发说明

--- src/handlers/home_handler.rs
+++ src/handlers/home_handler.rs
@@ -4,6 +4,10 @@ type ResultWarp<T> = std::result::Result<T, Rejection>;
 
 /* 响应/请求的返回 */
 pub async fn index() -> ResultWarp<impl Reply> {
+    log::debug!("[调试信息]访问了“/”");
+    log::warn!("[警告信息] warn");
+    log::info!("[提示信息] info");
+    
     let html = "欢迎访问首页(Hi Luck)";
     Ok(warp::reply::html(html))    //直接返回html
     // Err(warp::reject::not_found())   //错误的返回
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::common::get_env;
+use std::io::Write;
 use std::net::SocketAddr;
 
 mod common;
@@ -11,7 +12,25 @@ extern crate diesel;
 
 #[tokio::main]
 async fn main() {
-    println!("跟我买车-api!");
+    //
+    let log_level = get_log_level();
+
+    println!("跟我买车-api日志记录级别：{}", log_level);
+
+    //设置算定义日志记录格式  env_logger::init();  //RUST_LOG=debug cargo run
+    env_logger::Builder::new()
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} [{}] - {}",
+                chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .filter(None, log_level)
+        .init();
+
     let routes = filters::all_routes();
 
     //取得https证书等
@@ -20,8 +39,8 @@ async fn main() {
     let ip_addr = get_env("ip_address");
     let socket_addr: SocketAddr = ip_addr.as_str().parse().unwrap();
 
-    println!("站点（{}）", get_env("BASE_URL"));
-    println!("warp https 监听： {:?}", ip_addr);
+    log::info!("跟我买车-api: （{}）", get_env("BASE_URL"));
+    log::info!("warp https 监听： {:?}", ip_addr);
 
     warp::serve(routes)
         .tls()
@@ -29,4 +48,24 @@ async fn main() {
         .key_path(key_path)
         .run(socket_addr)
         .await;
+}
+
+fn get_log_level() -> log::LevelFilter {
+    let level = get_env("RUST_LOG");
+
+    let mut log_level = log::LevelFilter::Debug;
+    if level.eq("Debug") {
+        log_level = log::LevelFilter::Debug;
+    } else if level.eq("Info") {
+        log_level = log::LevelFilter::Info;
+    } else if level.eq("Warn") {
+        log_level = log::LevelFilter::Warn;
+    } else if level.eq("Error") {
+        log_level = log::LevelFilter::Error;
+    } else if level.eq("Off") {
+        log_level = log::LevelFilter::Off;
+    } else if level.eq("Trace") {
+        log_level = log::LevelFilter::Trace;
+    }
+    log_level
 }
